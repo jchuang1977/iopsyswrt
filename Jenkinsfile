jchuang1977/iopsyswrt

commit fca4d67169b16254505e5c3b765e7c094712ce27

jenkins: better globing rule.
makes it clearer what we are doing and no risk of uploading the wrong file.

Jenkinsfile
@@ -450,7 +450,7 @@ def our_stages(boards){
 				def img_upload_path = "${IOPSYS_FIRMWARE_PATH}/${build_type}/IOP${iopsys_version}/${dirname}/"
 
 				/* Used for stage Test */
-				def img_filename = sh (script: 'basename $(ls -t ./bin/targets/'+target+'/'+subtarget+'/'+filename+'-*.[yp]* | head -n1)', returnStdout: true).trim()
+				def img_filename = sh (script: 'basename $(ls -t ./bin/targets/'+target+'/'+subtarget+'/'+filename+'-*.+(y3|pkgtb) | head -n1)', returnStdout: true).trim()
 				img_server_path = "${IOPSYS_FIRMWARE_URL}/${build_type}/IOP${iopsys_version}/${dirname}/${img_filename}"
 
 				if ( customer != "" ) {

commit 38b877a7bb61c3f1bbee39d589ca226d8c5ff3d2

jenkins: fix firmware upload glob pattern
Issue introduced in fca4d67169b16254505e5c3b765e7c094712ce27

Jenkinsfile
@@ -450,7 +450,7 @@ def our_stages(boards){
 				def img_upload_path = "${IOPSYS_FIRMWARE_PATH}/${build_type}/IOP${iopsys_version}/${dirname}/"
 
 				/* Used for stage Test */
-				def img_filename = sh (script: 'basename $(ls -t ./bin/targets/'+target+'/'+subtarget+'/'+filename+'-*.+(y3|pkgtb) | head -n1)', returnStdout: true).trim()
+				def img_filename = sh (script: 'basename $(ls -1t ./bin/targets/'+target+'/'+subtarget+'/'+filename+'-*.(y3|pkgtb) | head -n1)', returnStdout: true).trim()
 				img_server_path = "${IOPSYS_FIRMWARE_URL}/${build_type}/IOP${iopsys_version}/${dirname}/${img_filename}"
 
 				if ( customer != "" ) {