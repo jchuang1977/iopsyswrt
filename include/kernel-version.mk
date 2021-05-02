50e848c05e52aa6125bbe680f3217ac0181a5cc0
Sukru Senli <sukru.senli@iopsys.eu>
2021-05-02 16:33:57 +0200

    kernel-version.mk: fix hash for 5.4.111 and remove 4.14.171

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -8,8 +8,7 @@ endif
 
 LINUX_VERSION-5.4 = .111
 
-LINUX_KERNEL_HASH-5.4.111 = 21626132658dc34cb41b7aa7b80ecf83751890a71ac1a63d77aea9d488271a03
-LINUX_KERNEL_HASH-4.14.171 = 8c46e833b4ff2bd2d4f35472e55f82c677d84d2373dbbfa6400825e7ced60663
+LINUX_KERNEL_HASH-5.4.111 = 4e1f01151f5662cb57742638100e52982a72eb61cc108833c557b36fadb73ec7
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
