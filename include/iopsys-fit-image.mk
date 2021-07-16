d38e4df26982a722b245169985b15c1a076edf56
Oussama Ghorbel <oussama.ghorbel@iopsys.eu>
2021-07-16 16:08:30 +0200

    added common code for iopsys FIT upgrade image
    Also some common code for target device model

---
+++ include/iopsys-fit-image.mk
@@ -0,0 +1,33 @@
+BUILD_DATE := $(shell date '+%y%m%d_%H%M')
+IOPSYS_VERSION_SUFFIX := $(subst ",,-X-$(CONFIG_TARGET_CUSTOMER)-$(CONFIG_TARGET_VERSION)-$(BUILD_DATE))
+
+# $$(1) contains the filesystem name
+# $$(2) contains the image name suffix as configured by the variable IMAGE/xxx
+# IMAGE_NAME specifies the target image file name
+define Device/iopsys-common
+	IMAGE_NAME = $$(DEVICE_MODEL)$(IOPSYS_VERSION_SUFFIX)-$$(1)$$(2)
+	IOPSYS_BUILD_VERSION = $$(DEVICE_MODEL)$(IOPSYS_VERSION_SUFFIX)
+endef
+# new introduced variables to Device/XXX should be added to DEVICE_VARS
+DEVICE_VARS += IOPSYS_BUILD_VERSION
+
+# $(1) target rootfs
+define iopsys-install-release-info
+	mkdir -p $(1)/etc/board-db/version
+	echo $(IOPSYS_BUILD_VERSION)   > $(1)/etc/board-db/version/iop_version
+	echo $(CONFIG_TARGET_CUSTOMER) > $(1)/etc/board-db/version/iop_customer
+endef
+
+# $(1) output file - the fit image
+# $(N) fit component name
+# $(N+1) fit component file
+define iopsys-fit-upgrade-image
+	CREATION_DATE="$(subst _, ,$(BUILD_DATE))" \
+    MODEL="$(DEVICE_MODEL)" \
+    IOPSYS_VERSION="$(IOPSYS_BUILD_VERSION)" \
+    $(TOPDIR)/scripts/mkits-iopsys-upgrade-image.sh \
+    $(1).its $(2) $(3) $(4) $(5) $(6) $(7) $(8) $(9)
+
+	PATH=$(LINUX_DIR)/scripts/dtc:$(PATH) mkimage -f $(1).its $(1).new
+	@mv $(1).new $(1)
+endef
---
+++ scripts/mkits-iopsys-upgrade-image.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+#
+# Generate FIT source for IOPSYSWRT upgrade image
+
+usage() {
+	echo "Usage: `basename $0` output img0_name img0_file [[img1_name img1_file] ...]"
+	exit 1
+}
+
+# We need at least 3 arguments
+[ "$#" -lt 3 ] && usage
+
+# Target output file
+OUTPUT="$1"; shift
+
+# Create a default, fully populated DTS file
+echo "\
+/dts-v1/;
+
+/ {
+	description = \"IOPSYSWRT FIT upgrade image\";
+	#address-cells = <1>;
+
+	id = \"IOPSYS\";
+	created = \"${CREATION_DATE}\";
+	model = \"${MODEL}\";
+	iopsys_version = \"${IOPSYS_VERSION}\";
+
+	images {" > ${OUTPUT}
+
+while [ -n "$1" -a -n "$2" ]; do
+	[ -f "$2" ] || usage
+
+	name="$1"; shift
+	file="$1"; shift
+
+	echo \
+"		${name} {
+			description = \"${name}\";
+			data = /incbin/(\"${file}\");
+			type = \"Firmware\";
+			arch = \"ARM\";
+			compression = \"none\";
+			hash@1 {
+				algo = \"crc32\";
+			};
+		};" >> ${OUTPUT}
+done
+
+echo \
+"	};
+};" >> ${OUTPUT}
